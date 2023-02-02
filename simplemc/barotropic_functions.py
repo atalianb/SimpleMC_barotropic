1a10fe55c14cf4c2385c6d76d1bf97a9c50e9f9c
Atalia Navarro-Boullosa <atalia.navarro.boullosa@gmail.com>
2023-02-02 16:22:50 -0600

    simpleMC modified with the barotroppic case

---
+++ simplemc/barotropic_functions.py
@@ -0,0 +1,66 @@
+import numpy as np
+from scipy import interpolate
+###
+##Constants
+####
+Gm=2.5188027*10**(-3)
+Gp=0.499246*10**(-6)
+G=4.258*10**(4)
+Gv=1.24*10**(5)
+y0=np.zeros(2)
+h=0.01
+x0=0.00001
+###
+##Initial condition
+y0[0]=0.#no cambia,
+######
+def Integrate(func,x0,y0,x,h):
+    # Finds value of y for a given x using step size h 
+    # and initial value y0 at x0.
+    def RK4(func,x0,y0,h):
+        K0 = h*func(x0,y0)
+        K1 = h*func(x0 + 0.5*h, y0 + 0.5*K0)
+        K2 = h*func(x0 + 0.5*h, y0 + 0.5*K1)
+        K3 = h*func(x0 + h, y0 + K2)
+        return (K0 + 2.*K1 + 2.*K2 + K3)/6.
+    X = []
+    Y = []
+    X.append(x0)
+    Y.append(y0)
+    #while x0 < x:
+    while y0[0] < 4.90000000000:
+    #while y0[0]>0.99:
+        y0 = y0 + RK4(func,x0,y0,h) #update next value of y
+        x0 = x0 + h #update next value of x
+        X.append(x0)
+        Y.append(y0)
+    return np.array(X),np.array(Y)
+
+def model(data,v,X):
+    V = []
+    x=X
+    RC=v*np.sqrt(G)
+    Int_l = interpolate.interp1d(x,RC)
+    for i in range(len(data)):
+        if data[i] < x[-1]:
+            v=Int_l(data[i])
+            V.append(v)
+        else:
+            #v=0.
+            v=np.sqrt(G*Y[:,0][-1]/(data[i]))
+            V.append(v)
+    return np.array(V)
+
+def Vc(r,kappa,rho0):
+    #ec de estado P=kap*rho
+    K = kappa
+    y0[1] = K*rho0
+    def rhs(x,y):
+        dy=np.zeros(2)
+        dy[0]=4*np.pi*Gm*x**2*(y[1]/K)
+        dy[1]=-Gp*y[0]/x**2*(y[1]/K)
+        return dy
+    X,Y=Integrate(rhs,x0,y0,10.,h)
+    v=np.sqrt(Y[:,0]/X)
+    V=model(r,v,X)
+    return V
---
+++ simplemc/models/RotationCurves.py
@@ -0,0 +1,113 @@
+
+from simplemc.cosmo.paramDefs import Anfw_par, rs_par#, eta_par, phi0_par, phi1_par, phi2_par
+from simplemc.barotropic_functions import *
+import numpy as np
+#from scipy import interpolate
+
+
+class RotationCurves():
+    def __init__(self, varya= True, varyb= True):#,varyc=True):#,varyd=True,varye=True):
+        """
+        Class to constrain rotational curves profiles,
+            Here we assume a NFW profile
+        Parameters
+        ----------
+        varya
+        varyb
+
+        Returns
+        -------
+
+        """
+        ## Example used to fit a straigth line two parameters: a, b
+        self.varya = varya
+        self.varyb = varyb
+        #self.varyc = varyc
+        #self.varyd = varyd
+        #self.varye = varye
+
+        self.Anfw = Anfw_par.value
+        self.rs = rs_par.value
+        #self.eta = eta_par.value
+        #self.phi1 = phi1_par.value
+        #self.phi2 = phi2_par.value
+
+        data_path = "simplemc/data/Blok_McGaugh_&_Rubin_(2001)/"
+        data = np.loadtxt(data_path+'ESO3020120.dat')
+        self.vecRp_data = np.array([row[1] for row in data])
+
+
+    # my free params (parameters/priors see ParamDefs.py)
+    def freeParameters(self):
+        l = []
+        if (self.varya): l.append(Anfw_par)
+        if (self.varyb): l.append(rs_par)
+        #if (self.varyc): l.append(eta_par)
+       # if (self.varyd): l.append(phi1_par)
+        #if (self.varye): l.append(phi2_par)
+        return l
+
+    def printFreeParameters(self):
+        print("Free parameters and values currently accepted:")
+        self.printParameters(self.freeParameters())
+
+
+    def printParameters(self, params):
+        for p in params:
+            print(p.name, '=' , p.value , '+/-' , p.error)
+
+
+    def updateParams(self, pars):
+        x = self.vecRp_data
+        for p in pars:
+            if p.name == "Anfw":
+                self.Anfw = p.value
+            elif p.name == "rs":
+                self.rs = p.value
+            #elif p.name == "eta":
+            #    self.eta = p.value
+            #elif p.name == "phi1":
+            #    self.phi1 = p.value
+            #elif p.name == "phi2":
+            #    self.phi2 = p.value
+        A = self.Anfw
+        rs = self.rs
+        #eta = self.eta
+        #phi1 = self.phi1
+        #phi2 = self.phi2
+        kappa = 10.**(A)
+        rho0 = rs
+        #eta = eta
+        #phi1 = 10.**(phi1)
+        #phi2 = 10.**(phi2)
+       # Vc = Vc_(x,m_a,eps,eta)
+       # self.f = interpolate.interp1d(Vc[0],Vc[1],fill_value='extrapolate')
+        return True
+
+
+    #def rotation(self,x):
+        #def nfw(r, rs =1, A=0.05):
+    #    A  = self.Anfw
+    #    rs = self.rs
+    #    return (A**2*(rs**3)/x)*(np.log((rs+x)/rs) - x/(rs+x))
+
+
+    def rotation(self,x):
+        #A = self.Anfw
+        #rs = self.rs
+        #phi0 = self.phi0
+        #phi1 = self.phi1
+        #phi2 = self.phi2
+
+        #m_a = 10.**(A)
+        #eps0 = 10.**(rs)
+        #phi0 = 10.**(phi0)
+        #phi1 = 10.**(phi1)
+        #phi2 = 10.**(phi2)
+        #Vc2 = Vc_inter(x,m_a,eps0,phi0,phi1,phi2)
+        #Vc_new = self.f(x)
+        Vc_new = Vc(x,kappa,rho0)
+        return np.sqrt(Vc_new)
+
+    def prior_loglike(self):
+        return 0 
